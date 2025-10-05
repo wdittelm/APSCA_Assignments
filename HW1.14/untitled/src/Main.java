41b9e7d7a3134e3bdbcf5f1d2a5866a1e5bf5b91
wdittelm <wdittelm@icloud.com>
2025-10-04 22:15:04 -0400

    haha

--- HW1.14/untitled/src/Main.java
+++ HW1.14/untitled/src/Main.java
@@ -1,16 +1,15 @@
-import //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         double sum = 0;
         int i = 1;
         int n = 10000;
         while (i<n){
-            sum = sum + (i/(i*i));
+            sum = sum + (1.0/(i*i));
             i += 1;
         }
         System.out.println(sum);
-        System.out.println(sum- (math.pi));
+        System.out.println(sum- (Math.PI*Math.PI));
 
 
     }
