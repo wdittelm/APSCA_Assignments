1458b3b1733274fd50517306159241514525857d
wdittelm <wdittelm@icloud.com>
2025-10-04 22:11:05 -0400

    haha

---
+++ HW1.14/untitled/src/Main.java
@@ -0,0 +1,18 @@
+import //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double sum = 0;
+        int i = 1;
+        int n = 10000;
+        while (i<n){
+            sum = sum + (i/(i*i));
+            i += 1;
+        }
+        System.out.println(sum);
+        System.out.println(sum- (math.pi));
+
+
+    }
+
+}
